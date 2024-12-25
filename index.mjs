5c9967cd8cd051e3a1af7790bb622e6b1394453c
jmhs11 <josemariahs@gmail.com>
2024-12-25 23:19:22 +0100

    Refactor manga scraping logic and add file output functionality

--- index.mjs
+++ index.mjs
@@ -1,47 +1,59 @@
+import fs from "node:fs";
 import { chromium } from "playwright";
 
-const browser = await chromium.launch({
-	headless: true,
-});
+(async () => {
+	const browser = await chromium.launch({
+		headless: true,
+	});
 
+	const context = await browser.newContext();
+	const page = await context.newPage();
+	page.setDefaultNavigationTimeout(600_000);
 
-const page = await browser.newPage();
-page.setDefaultNavigationTimeout(600_000)
+	await page.goto("https://www.listadomanga.es/lista.php");
 
-await page.goto("https://www.listadomanga.es/lista.php");
+	const mangaList = await page.$$eval("[href^='coleccion.php?id=']", (results) =>
+		results.map((result) => ({
+			title: result.textContent,
+			url: result.href,
+		}))
+	);
 
-// const mangaList = await page.$$eval("[href^='coleccion.php?id=']", (results) =>
-// 	results.map((result) => ({
-// 		title: result.textContent,
-// 		url: result.href,
-// 	}))
-// );
+	const genreList = await page.$$eval("[href^='lista.php?genero=']", (results) =>
+		results.map((result) => ({
+			title: result.textContent,
+			url: result.href,
+		}))
+	);
 
-const genreList = await page.$$eval("[href^='lista.php?genero=']", (results) =>
-	results.map((result) => ({
-		title: result.textContent,
-		url: result.href,
-	}))
-);
+	const test = await Promise.all(
+		genreList.map(async (genre) => {
+			const p = await browser.newPage();
+			await p.goto(genre.url);
 
-const test = await Promise.all(
-  genreList.map(async (genre) => {
-		await page.goto(genre.url, { waitUntil: "domcontentloaded" });
+			const genreWithMangas = await page.$$eval("[href^='coleccion.php?id=']", (results) => {
+				results.map((result) => {
+					console.log(result)
+					// title: result.textContent,
+					// url: result.href,
+				});
+			});
 
-		const genreWithMangas = await page.$$eval("[href^='coleccion.php?id=']", (results) => {
-			return results.map((result) => ({
-				title: result.textContent,
-				url: result.href,
-			}));
-		});
+			p.close();
 
-		return { ...genre, list: genreWithMangas };
-	})
-);
+			return { ...genre, list: genreWithMangas };
+		})
+	);
 
-// const mergedList = [{ id: 1, title: "Manga", list: mangaList }, ...genreList];
+	const mergedList = [{ id: 1, title: "Manga", list: mangaList }, ...test];
 
-// console.log(mergedList);
+	await browser.close();
 
-console.log(test);
-await browser.close();
+	fs.writeFile("./mangas.json", JSON.stringify(mergedList), "utf8", (err) => {
+		if (err) {
+			console.error(err);
+		} else {
+			console.log("File written successfully");
+		}
+	});
+})();
