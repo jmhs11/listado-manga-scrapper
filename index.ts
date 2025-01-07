c089791f850b628e2216913ceecfafb8710e86f5
jmhs11 <josemariahs@gmail.com>
2025-01-07 20:52:32 +0100

    Enhance manga data extraction by adding new properties and improve cookie acceptance handling

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import retry from "async-retry";
 
 const baseURL = "https://www.listadomanga.es";
 
+let counterManga = 0;
+
 const getPropertyByKey = (key: string) => {
 	switch (key) {
 		case "Título original":
@@ -26,19 +28,141 @@ const getPropertyByKey = (key: string) => {
 			return "numOfJapaneseVolumes";
 		case "Números en castellano":
 			return "numOfSpanishVolumes";
+		case "Números en catalán":
+			return "numOfCatalanVolumes";
+		case "Traducción":
+			return "translation";
+		case "Color":
+			return "color";
+		case "Edición original":
+			return "originalEdition";
+		case "Historia original":
+			return "originalStory";
+		case "Producción":
+			return "production";
+		case "Diseño de personajes":
+			return "characterDesign";
+		case "Colaborador":
+			return "collaborator";
+		case "Supervisión":
+			return "supervision";
+		case "Asistente de composición":
+			return "compositionAssistant";
+		case "Ilustraciones":
+			return "illustrations";
+		case "Historia":
+			return "story";
+		case "Editorial taiwanesa":
+			return "taiwanesePublisher";
+		case "Números en taiwanés":
+			return "numOfTaiwaneseVolumes";
+		case "Números en chino":
+			return "numOfChineseVolumes";
+		case "Editorial china":
+			return "chinesePublisher";
+		case "Nota":
+			return "note";
+		case "Editorial francesa":
+			return "frenchPublisher";
+		case "Números en francés":
+			return "numOfFrenchVolumes";
+		case "Números en malayo":
+			return "numOfMalayVolumes";
+		case "Asesor":
+			return "advisor";
+		case "Editorial vietnamita":
+			return "vietnamesePublisher";
+		case "Números en vietnamita":
+			return "numOfVietnameseVolumes";
+		case "Números en coreano":
+			return "numOfKoreanVolumes";
+		case "Editorial surcoreana":
+			return "koreanPublisher";
+		case "Editorial americana":
+			return "americanPublisher";
+		case "Números en inglés":
+			return "numOfEnglishVolumes";
+		case "Números en gallego":
+			return "numOfGalicianVolumes";
+		case "Adaptación":
+			return "adaptation";
+		case "Autores":
+			return "authors";
+		case "Números en español":
+			return "numOfSpanishVolumes";
+		case "Escenarios":
+			return "scenarios";
+		case "Números en valenciano":
+			return "numOfValencianVolumes";
+		case "Storyboard":
+			return "storyboard";
+		case "Entintado":
+			return "inking";
+		case "Editorial inglesa":
+			return "englishPublisher";
+		case "Números en asturiano":
+			return "numOfAsturianVolumes";
+		case "Editorial canadiense":
+			return "canadianPublisher";
+		case "Tinta":
+			return "ink";
+		case "Creador":
+			return "creator";
+		case "Asistentes":
+			return "assistants";
+		case "Editorial italiana":
+			return "italianPublisher";
+		case "Números en italiano":
+			return "numOfItalianVolumes";
+		case "Números en alemán":
+			return "numOfGermanVolumes";
+		case "Editorial alemana":
+			return "germanPublisher";
+		case "Números en euskera":
+			return "numOfBasqueVolumes";
+		case "Portada":
+			return "cover";
+		case "Fotografía":
+			return "photography";
+		case "Números en sueco":
+			return "numOfSwedishVolumes";
+		case "Números en portugués brasileño":
+			return "numOfBrazilianPortugueseVolumes";
+		case "Texto":
+			return "text";
+		case "Editor":
+			return "editor";
+		case "Novela":
+			return "novel";
+		case "Idea Original":
+			return "originalIdea";
+		case "Números en tailandés":
+			return "numOfThaiVolumes";
+		case "Prólogo":
+			return "prologue";
+		case "Narración":
+			return "narration";
+		case "Números en hindi":
+			return "numOfHindiVolumes";
+		case "Números en tailandés":
+			return "numOfThaiVolumes";
+		case "Números en tailandés":
+			return "numOfThaiVolumes";
+		case "Números en tailandés":
+			return "numOfThaiVolumes";
 		default:
 			return key;
 	}
-}
+};
 
 const acceptCookies = async (page: Page, noConsoleLog: boolean = false) => {
-	const isCookiesBannerVisible = await page.isVisible(".qc-cmp2-summary-buttons")
+	const isCookiesBannerVisible = await page.isVisible(".qc-cmp2-summary-buttons");
 
 	if (!isCookiesBannerVisible) return;
 
-	if (!noConsoleLog) console.log("Aceptando Cookies de la nueva página")
+	if (!noConsoleLog) console.log("Aceptando Cookies de la nueva página");
 	await page.getByRole("button", { name: "ACEPTO" }).click();
-}
+};
 
 const getMangaData = async (page: Page, browser: Browser) => {
 	const mangaGenre = {
@@ -55,17 +179,20 @@ const getMangaData = async (page: Page, browser: Browser) => {
 		}))
 	);
 
-	const genreList = [mangaGenre, ...genreRest].map((genre) => ({ ...genre, url: `${baseURL}/${genre.url}` })).sort((a, b) => a.id - b.id);
+	const genreList = [...genreRest, mangaGenre]
+		.map((genre) => ({ ...genre, url: `${baseURL}/${genre.url}` }))
+		.sort((a, b) => a.id - b.id);
 
 	for (const genre of genreList) {
 		console.log("Navegando a la página del género", genre.title);
 		const page = await browser.newPage();
+		page.setDefaultNavigationTimeout(600_000);
 
 		await page.goto(genre.url);
 
 		await acceptCookies(page);
 
-		console.log("Capturando pantalla")
+		console.log("Capturando pantalla");
 		await page.screenshot({ path: `./screenshots/genres/${genre.title}.png`, fullPage: true });
 
 		const mangaList = await page.$$eval("[href^='coleccion.php?id=']", (results) =>
@@ -85,11 +212,12 @@ const getMangaData = async (page: Page, browser: Browser) => {
 				{
 					name: "mostrarNSFW",
 					value: "true",
-					url: baseURL
-				}
+					url: baseURL,
+				},
 			]);
 
 			const page = await context.newPage();
+			page.setDefaultNavigationTimeout(600_000);
 
 			await page.goto(manga.url);
 
@@ -99,22 +227,26 @@ const getMangaData = async (page: Page, browser: Browser) => {
 			// await page.screenshot({ path: `./screenshots/mangas/${manga.title}.png`, fullPage: true });
 
 			const $data = await page.locator(".izq").first().allInnerTexts();
+			// const $data2 = await Promise.allSettled((await page.locator(".izq").first().all()).map((el) => el.innerText()));
+			// console.log($data2);
 
-			const mangaData = $data[0].split("\n").reduce((acc, cur) => {
-				if (cur.includes(":")) {
-					const [key, value] = cur.split(":")
-					acc[getPropertyByKey(key)] = value.trim();
-				} else if (!cur.includes(":")) {
-					acc["title"] = cur;
-				} else {
-					acc[cur] = cur;
+			const mangaData = $data[0].split("\n").reduce((acc, cur, index) => {
+				// console.log(index, ":", cur)
+				if (index === 0) acc["title"] = cur;
+				else {
+					const [key, value] = cur.split(/:(.*)/s);
+					// console.log(key, ":", value);
+					acc[getPropertyByKey(key)] = value?.trim() ?? "";
 				}
+
 				return acc;
 			}, {} as Record<string, string>);
 
 			mangaCollection = [...mangaCollection, mangaData];
 
 			await page.close();
+
+			counterManga++;
 		}
 
 		writeFileSync(`./data/${genre.title}.json`, JSON.stringify(mangaCollection, null, 2));
@@ -131,13 +263,13 @@ async function main() {
 	const browser = await chromium.launch();
 
 	const page = await browser.newPage();
+	page.setDefaultNavigationTimeout(600_000);
 
 	try {
 		console.log("Navegando a la página");
-		await page.goto("https://www.listadomanga.es/lista.php", { timeout: 600_000 });
+		await page.goto("https://www.listadomanga.es/lista.php");
 
-		console.log("Aceptando cookies");
-		await page.getByRole("button", { name: "ACEPTO" }).click();
+		acceptCookies(page);
 
 		console.log("Obteniendo lista de mangas");
 		await getMangaData(page, browser);
@@ -145,6 +277,7 @@ async function main() {
 		throw err;
 	} finally {
 		await browser.close();
+		console.log("Mangas Scrapeados: ", counterManga);
 	}
 }
 
